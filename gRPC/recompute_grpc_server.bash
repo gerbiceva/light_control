5ed76503c1d178dc05cc545c74f0feec84e8e5fc
Lan Vukušič <lan.vukusic@gmail.com>
2025-01-15 15:00:16 +0100

    grpc server build fixed

--- gRPC/recompute_grpc_server.bash
+++ gRPC/recompute_grpc_server.bash
@@ -1,1 +1,7 @@
-python3 -m grpc_tools.protoc -I. --python_out=../src/grpc_server --pyi_out=../src/grpc_server --grpc_python_out=../src/grpc_server service.proto
+export GRPC_SERVER_PATH="../src/grpc_server"
+export PYTHON_PKG="grpc_server"
+
+python3 -m grpc_tools.protoc -I. --python_out=$GRPC_SERVER_PATH --pyi_out=$GRPC_SERVER_PATH --grpc_python_out=$GRPC_SERVER_PATH service.proto
+
+# Use sed to replace the import statement with the correct package
+sed -i "s|import service_pb2 as service__pb2|import ${PYTHON_PKG}.service_pb2 as service__pb2|g" $GRPC_SERVER_PATH/service_pb2_grpc.py
