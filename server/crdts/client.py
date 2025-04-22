09463d10cef4a70047a73f71006d987d8a972f0d
Lan Vukušič <lan.vukusic@gmail.com>
2025-04-22 11:59:27 +0200

    sync backend frontend with CRDTs

--- server/crdts/client.py
+++ server/crdts/client.py
@@ -1,29 +1,53 @@
 from httpx_ws import aconnect_ws
-from pycrdt import Doc, Array, Map, ArrayEvent
+from pycrdt import Doc, Array, Map, MapEvent
 from pycrdt_websocket import WebsocketProvider
 import asyncio
 from pycrdt_websocket.websocket import HttpxWebsocket
 
-ydoc = Doc()  # dont ever at all under any circumstance observe this fucker.
-yarray = ydoc.get("count", type=Array)
+
+ydoc = Doc()
+syncedAppState = ydoc.get("syncedAppState", type=Map)
 
 
-def cb(event: ArrayEvent):
-    try:
-        print(event)
-        pass
-
-    except Exception as e:
-        print("except", e)
+def cb(events):
+    for event in events:
+        event: MapEvent = event
+        print(f"Target: {event.target}")
 
 
 async def client(port: int, room_name: str):
     async with (
         aconnect_ws(f"ws://localhost:{port}/{room_name}") as websocket,
-        WebsocketProvider(ydoc, HttpxWebsocket(websocket, room_name)),
+        WebsocketProvider(ydoc, HttpxWebsocket(websocket, room_name)) as provider,
     ):
-        # Changes to remote ydoc are applied to local ydoc.
-        # Changes to local ydoc are sent over the WebSocket and
-        # broadcast to all clients.
-        yarray.observe(cb)
+        print(
+            "Connection state:",
+            websocket.connection.state,
+            "started" if provider._started else "not started",
+        )
+
+        # Initialize the state using dict notation
+        with ydoc.transaction():
+            syncedAppState["state"] = map0 = Map(
+                {
+                    "subgraphs": Map(),
+                    "main": Map(
+                        {
+                            "id": 0,
+                            "name": "main",
+                            "description": "This main flow gets executed always",
+                            "nodes": Array(),
+                            "edges": Array(),
+                        }
+                    ),
+                }
+            )
+
+        print("Initialized state with CRDT types:")
+        print(map0.to_py())
+
+        # Observe changes
+        syncedAppState.observe_deep(cb)
+
+        # Keep the connection open
         await asyncio.Future()
